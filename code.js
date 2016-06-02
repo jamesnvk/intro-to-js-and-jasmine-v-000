0331dbaf993e58e2b8c44b55a9e58325f87dd8e3
James Novak <jamesnovak90@gmail.com>
2016-06-02 00:13:53 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(name) {
+  return "I love " + name
+}
+
+function shouting(stuff) {
+  return stuff.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true
+}
